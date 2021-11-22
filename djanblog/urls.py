49726ed9aa9d7cb39c13b3b3c93e92329f448b99
Kennedy <kennedy.lucas@gmail.com>
2021-11-22 10:43:26 -0300

    Modificado URL pra adicionar o prefixo api

--- djanblog/urls.py
+++ djanblog/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('api/blog/', include('blog.urls')),
     path('tinymce/', include('tinymce.urls'))
 ]
